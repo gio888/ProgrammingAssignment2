8b1c301d408c705704e5973effeebf0de4b258ef
gio888 <gbacareza@gmail.com>
2014-05-21 23:57:43 +0800

    Solution coded
    Coded my solution to Programming Exercise 2

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,40 @@
-## Put comments here that give an overall description of what your
-## functions do
+## I basically modeled the behavior from the sample.
 
-## Write a short comment describing this function
+## As with the sample, makeCacheMatrix creates a list containing a function to
+## set and the get the value of the matrix
+## get and set the value of the inverse matrix
 
 makeCacheMatrix <- function(x = matrix()) {
+        inv <- NULL
+        set <- function(y) {
+                x <<- y
+                inv <<- NULL
+        }
+        get <- function() x
+        setinv <- function(solve) inv <<- solve
+        getinv <- function() inv
+        list(set = set, get = get,
+             setinv = setinv,
+             getinv = getinv)
 
 }
 
 
-## Write a short comment describing this function
+## Again modeling from the sample, cacheSolve basically calculates the inverse
+## using the built in solve() function.
+## As with sample, it checks if inverse has already been calculated and returns that.
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        inv <- x$getinv()
+        if(!is.null(inv)) {
+                message("getting cached data")
+                return(inv)
+        }
+        data <- x$get()
+        ## introducing matrix id as the identity matrix of the parameter matrix
+        id <- diag(nrow(data))
+        inv <- solve(data, id, ...)
+        x$setinv(inv)
+        inv
 }
